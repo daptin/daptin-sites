50eb0597a2fb13173ca9d1b7fb2ad4f3d2df5482
artpar <artpar@gmail.com>
2019-09-30 01:21:24 +0530

    preload user account model

--- src/api/index.js
+++ src/api/index.js
@@ -9,11 +9,11 @@ console.log("end point from localstorage", endpoint);
 console.log("end point from env", process.env.DAPTIN_ENDPOINT);
 
 const daptinClient = new DaptinClient(endpoint || process.env.DAPTIN_ENDPOINT, logRequests);
-// daptinClient.worldManager.loadModels().then(function () {
+daptinClient.worldManager.loadModel("user_account").then(function () {
   // daptinClient.jsonApi.findAll("user_account").then(function (res) {
   //   console.log("all todos", res.data)
   // })
-// });
+});
 
 export default daptinClient;
 
